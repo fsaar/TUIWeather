829c4a58b33b88d5c20502b5d05508bb25f4442c
Frank Saar <frank.saar@gmail.com>
2018-05-11 23:02:42 +0100

    added test target

--- TUIWeather/TUIWeather/Network/RequestManager.swift
+++ TUIWeather/TUIWeather/Network/RequestManager.swift
@@ -4,15 +4,13 @@ enum RequestManagerErrorType : Error {
     case InvalidURL(urlString : String)
 }
 
-
-
 class RequestManager : NSObject {
     fileprivate let RequestManagerBaseURL = "https://api.openweathermap.org"
 
     fileprivate let ApplicationID = "ea7de0326863831395fe4487ac2fb3f1"
     public static let shared =  RequestManager()
 
-    fileprivate lazy var session = URLSession(configuration: .default)
+    lazy var session = URLSession(configuration: .default)
 
     public func getDataWithRelativePath(relativePath: String ,and query: [String]? = nil, completionBlock:@escaping ((_ data : Data?,_ error:Error?) -> Void)) {
         guard let url =  self.baseURL(withPath: relativePath,and: query) else {
@@ -25,7 +23,7 @@ class RequestManager : NSObject {
 
 
 // MARK : Private
-
+    
 extension RequestManager {
     fileprivate func getDataWithURL(URL: URL , completionBlock:@escaping ((_ data : Data?,_ error:Error?) -> Void)) {
         let task = session.dataTask(with: URL, completionHandler: { (data, _, error) -> (Void) in
